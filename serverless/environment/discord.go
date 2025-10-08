6a767285d7152bfa83677a9430a670a698e0b4bd
lausan3 <lauanthony4455@gmail.com>
2025-10-08 14:13:12 -0400

    feat: add discord api url to env

--- serverless/environment/discord.go
+++ serverless/environment/discord.go
@@ -4,4 +4,6 @@ type DiscordConfig struct {
 	AppID     string `mapstructure:"DISCORD_APP_ID"`
 	Token     string `mapstructure:"DISCORD_TOKEN"`
 	PublicKey string `mapstructure:"DISCORD_PUBLIC_KEY"`
+
+	APIUrl string `mapstructure:"DISCORD_API_URL"`
 }
